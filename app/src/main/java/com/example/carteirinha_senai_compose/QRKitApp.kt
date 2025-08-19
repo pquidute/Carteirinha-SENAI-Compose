7e377aea6215b2cf09d88bce3ecba97475fe0dac
Pedro Quidute <pquidute@proton.me>
2025-08-18 21:40:54 -0300

    QR code generator implemented

---
+++ app/src/main/java/com/example/carteirinha_senai_compose/QRKitApp.kt
@@ -0,0 +1,9 @@
+package com.example.carteirinha_senai_compose
+
+import android.app.Application
+
+class QRKitApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
